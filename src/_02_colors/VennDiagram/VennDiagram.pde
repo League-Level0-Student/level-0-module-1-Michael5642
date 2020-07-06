2ba03b4a8efee368890a50348f0a8aecfaa2df2c
root <root@dd799e08b23e>
2020-07-06 23:59:31 +0000

    level 0 module 1

--- src/_02_colors/VennDiagram/VennDiagram.pde
+++ src/_02_colors/VennDiagram/VennDiagram.pde
@@ -1,1 +1,14 @@
 size(600,600);
+textSize(50);
+//red circle
+fill(255,0,0,100);
+ellipse(300,200,100,100);
+text("Power",300,200);
+//green circle
+fill(0,255,0,100);
+ellipse(325,250,100,100);
+text("Courage",325,250);
+//blue circle
+fill(0,0,255,100);
+ellipse(275,250,100,100);
+text("Wisdom",275,250);
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,25 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
+String student1="Maria is a cool name";
+String student2="Michael is a weird name";
+String student3="Dylan is kind of a jerk name";
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
-		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
-	}
+String name=JOptionPane.showInputDialog("what is your name?");
+if(name.equalsIgnoreCase("Maria")) {
+JOptionPane.showMessageDialog(null, student1);
 }
+else if(name.equalsIgnoreCase("Michael")) {
+JOptionPane.showMessageDialog(null, student2);	
+}
+else if(name.equalsIgnoreCase("Dylan")) {
+JOptionPane.showMessageDialog(null, student3);	
+}
+else {
+JOptionPane.showMessageDialog(null, "go away...");	
+}
+		}
+	}
+// 3. In a pop-up, tell the user what is remarkable about that person.
 
